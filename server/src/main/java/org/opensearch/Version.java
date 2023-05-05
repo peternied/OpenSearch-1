82b5d178d5e2d95a09a5c5fc65832f2a2d824f03
Peter Nied <petern@amazon.com>
2023-05-05 01:28:07 +0000

    Increment version to 2.7.3 #66
    Signed-off-by: Peter Nied <petern@amazon.com>

--- server/src/main/java/org/opensearch/Version.java
+++ server/src/main/java/org/opensearch/Version.java
@@ -114,7 +114,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_2_7_0 = new Version(2070099, org.apache.lucene.util.Version.LUCENE_9_5_0);
     public static final Version V_2_7_1 = new Version(2070199, org.apache.lucene.util.Version.LUCENE_9_5_0);
     public static final Version V_2_7_2 = new Version(2070299, org.apache.lucene.util.Version.LUCENE_9_5_0);
-    public static final Version CURRENT = V_2_7_2;
+    public static final Version V_2_7_3 = new Version(2070399, org.apache.lucene.util.Version.LUCENE_9_5_0);
+    public static final Version CURRENT = V_2_7_3;
 
     public static Version readVersion(StreamInput in) throws IOException {
         return fromId(in.readVInt());
